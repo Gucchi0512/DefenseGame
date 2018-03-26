a8951cbf005d248d0ec1ed3c875f368c50f34622
森口大雅 <gucchi512@outlook.jp>
2018-03-26 15:07:20 +0900

    敵召喚
    四方より敵召喚完成

---
+++ Defencegame/Assets/Scripts/enemyspown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyspown : MonoBehaviour {
+    public GameObject gobrin;
+    public GameObject hobgobrin;
+    public GameObject wolf;
+    public GameObject troll;
+    int enemynum;
+    float spownspan=10.0f;
+    float passedtime=0.0f;
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        enemynum = Random.Range(0, 11);
+        passedtime += Time.deltaTime;
+        if (passedtime >= spownspan) {
+            passedtime = 0.0f;
+            if (enemynum >= 0 && enemynum < 5) {
+                Instantiate(gobrin, transform.position, Quaternion.identity);
+            } else if (enemynum < 8) {
+                Instantiate(hobgobrin, transform.position, Quaternion.identity);
+            } else if (enemynum < 10) {
+                Instantiate(wolf, transform.position, Quaternion.identity);
+            } else {
+                Instantiate(troll, transform.position, Quaternion.identity);
+            }
+        }
+	}
+}
