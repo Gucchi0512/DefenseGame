223ac5b71711a4b3b87ca74413fbc0d1ebefb4e8
森口大雅 <gucchi512@outlook.jp>
2018-03-27 15:35:53 +0900

    敵移動実装(ゴブリン) 移動のみ

---
+++ Defencegame/Assets/Scripts/gobrinmove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class gobrinmove : MonoBehaviour {
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.speed = 1;
+        agent.destination = target.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
