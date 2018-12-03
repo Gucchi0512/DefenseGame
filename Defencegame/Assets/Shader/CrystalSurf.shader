ba721252f16bacc585220964691a22e86db9ac05
森口大雅 <gucchi512@outlook.jp>
2018-12-04 08:26:48 +0900

    クリスタルが光る

--- Defencegame/Assets/Shader/CrystalSurf.shader
+++ Defencegame/Assets/Shader/CrystalSurf.shader
@@ -2,7 +2,7 @@
 	Properties{
 		_Color("Color", Color) = (1,1,1,1)
 		_AlphaRate("AlphaRate", Float) = 1.0
-
+		_EmissionColor("EmissionColor", Color)=(1,1,1,1)
 	}
 	SubShader {
 		Tags { "RenderType"="Transparent" "Queue"="Transparent" }
@@ -10,12 +10,13 @@
 		
 		Pass{
 			Zwrite On
-			ColorMask 0	
-		}
+			ColorMask 0
 
+		}
+			Cull Off
 		CGPROGRAM
 		// Physically based Standard lighting model, and enable shadows on all light types
-		#pragma surface surf Standard alpha:fade
+		#pragma surface surf Standard Lambert alpha:fade
 
 		// Use shader model 3.0 target, to get nicer looking lighting
 		#pragma target 3.0
@@ -27,11 +28,16 @@
 		
 		fixed4 _Color;
 		float _AlphaRate;
+		half4 _EmissionColor;
+
 		void surf (Input IN, inout SurfaceOutputStandard o) {
 			o.Albedo = _Color.rgb;
+			float t = ((2 * _SinTime.w*_CosTime.w) + 1.0)*0.5;
 			float alpha = 1 - (abs(dot(IN.viewDir, IN.worldNormal)));
-			if (alpha <= 0.3) alpha = 0.3;
+			if (alpha <= 0.3) alpha = 0.3 ;
+			float e = alpha * t;
 			o.Alpha = alpha * _AlphaRate;
+			o.Emission = _EmissionColor * e;
 		}
 		ENDCG
 	}
--- Defencegame/Assets/scripts/magic.cs
+++ Defencegame/Assets/scripts/magic.cs
@@ -3,19 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class magic : MonoBehaviour {
-    public GameObject bullet;
-    GameObject Magbullet;
-    float bulletsize = 0;
+    [SerializeField] private GameObject magicbullet;
+    private ParticleSystem particle;
+    private bool flag=false;//魔法の有り無し
+    private GameObject magbullet;
+    private float bulletsize=0;
 
-	// Use this for initialization
+    // Use this for initialization
 	void Start () {
-		
+        particle = magicbullet.GetComponentInChildren<ParticleSystem>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            Magbullet = Instantiate(bullet, this.transform.position, bullet.transform.rotation);
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0) && !flag) {
+            magbullet = Instantiate(magicbullet, this.transform.position, this.transform.rotation);
+            flag = true;
+        }else if (Input.GetMouseButton(0)) {
+            particle.Play();
+            bulletsize += 0.01f;
+            magbullet.transform.localScale = new Vector3(bulletsize, bulletsize, bulletsize);
+        } else {
+            particle.Stop();
+            if (flag) {
+                float speed = 2.0f;
+                Vector3 force = this.transform.forward * (speed - bulletsize);
+                magbullet.GetComponent<Rigidbody>().AddForce(force);
+                flag = false;
+            }
         }
     }
 }
