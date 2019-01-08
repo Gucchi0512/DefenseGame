43e268db9efaac63df0fc801ce9503eeb7a26008
森口大雅 <gucchi512@outlook.jp>
2019-01-09 00:07:06 +0900

    テレポート実装中

---
+++ Defencegame/Assets/Scripts/MovePointer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePointer: MonoBehaviour {
+	[SerializeField]private float range = 100f;
+	[SerializeField]private GameObject reticlePrefab;
+	[SerializeField]private Vector3 reticleOffset;
+
+	private GameObject reticle;
+	private int rayMask;
+
+	LineRenderer laser;
+	Ray ray;
+	RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+		rayMask=LayerMask.GetMask("movable");
+		laser=GetComponent<LineRenderer>();
+		reticle=Instantiate(reticlePrefab);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
